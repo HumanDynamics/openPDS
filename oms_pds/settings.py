f718e6e5fc2e9cfb001a74489bdfdfe01f9a0fe0
JDSchmitz <jdschmit@cord.edu>
2012-11-14 13:52:30 -0800

    changing db location

--- oms_pds/settings.py
+++ oms_pds/settings.py
@@ -21,9 +21,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'NAME': '/var/www/trustframework3/testPDS.db',                      # Or path to database file if using sqlite3.
+        'USER': 'test',                      # Not used with sqlite3.
+        'PASSWORD': 'test',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
