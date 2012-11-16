12809998438c02126a2714356886b5299b81abb4
JDSchmitz <jdschmit@cord.edu>
2012-11-15 22:31:43 -0800

    adding answer list to tools

--- oms_pds/pds/tools.py
+++ oms_pds/pds/tools.py
@@ -1,10 +1,12 @@
 from tastytools.api import Api
-from oms_pds.pds.api import FunfResource, FunfConfigResource, RoleResource, PurposeResource, AnswersResource
+from oms_pds.pds.api import FunfResource, FunfConfigResource, RoleResource, PurposeResource, AnswerResource, AnswerListResource
 
 v1_api = Api(api_name='personal_data')
 v1_api.register(FunfResource())
 v1_api.register(FunfConfigResource())
 v1_api.register(RoleResource())
 v1_api.register(PurposeResource())
-v1_api.register(AnswersResource())
+v1_api.register(AnswerResource())
+v1_api.register(AnswerListResource())
+
 
