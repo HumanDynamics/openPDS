6a01713964fe1692d15e222ed2bc39e0482da981
Brian <brian717@media.mit.edu>
2012-12-21 14:57:46 -0500

    Removed pdb trace from resources obj_get_list

--- oms_pds/tastypie_mongodb/resources.py
+++ oms_pds/tastypie_mongodb/resources.py
@@ -81,7 +81,7 @@ class MongoDBResource(Resource):
         """
         Maps mongodb documents to Document class.
         """
-        pdb.set_trace()
+#        pdb.set_trace()
         filter_object = self.get_filter_object(request)
         
         return map(Document, self.get_collection(request).find(filter_object))
