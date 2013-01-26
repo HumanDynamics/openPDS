934de84c91406aaceb57856d15ed746672f9d89a
brian717 <brian717@mit.edu>
2013-01-25 20:20:42 -0500

    Fixed endpoint for social health data

--- oms_pds/static/js/socialHealthRadial.js
+++ oms_pds/static/js/socialHealthRadial.js
@@ -12,7 +12,16 @@ window.AnswerList = Backbone.Model.extend({});
 
 window.AnswerListCollection = Backbone.Collection.extend({
 	model: AnswerList, 
-	urlRoot: ANSWERLIST_API_URL
+	urlRoot: ANSWERLIST_API_URL,
+	
+	fetch: function (options) {
+		options || (options = {});
+		options.data || (options.data = {});
+		filterMapping = { "key": "socialhealth" }
+		options.data = _.extend(options.data, filterMapping);
+		
+		return Backbone.Collection.prototype.fetch.call(this,options);
+	}
 });
 
 window.SocialHealthRadialView = Backbone.View.extend({
