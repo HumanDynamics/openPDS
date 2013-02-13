ec9b983f38c78931fb38be1cf343b98e7fe221f0
brian717 <brian717@mit.edu>
2013-02-13 17:54:07 -0500

    Work on spherical mercator projection for points in map.

--- oms_pds/static/js/answerListMap.js
+++ oms_pds/static/js/answerListMap.js
@@ -14,7 +14,35 @@ $(function () {
             return Backbone.Collection.prototype.fetch.call(this,options);
         }
     });
-    
+    function deg_rad(ang) {
+        return ang * (Math.PI/180.0)
+    }
+    function merc_x(lon) {
+        var r_major = 6378137.000;
+        return r_major * deg_rad(lon);
+    }
+    function merc_y(lat) {
+        if (lat > 89.5)
+            lat = 89.5;
+        if (lat < -89.5)
+            lat = -89.5;
+        var r_major = 6378137.000;
+        var r_minor = 6356752.3142;
+        var temp = r_minor / r_major;
+        var es = 1.0 - (temp * temp);
+        var eccent = Math.sqrt(es);
+        var phi = deg_rad(lat);
+        var sinphi = Math.sin(phi);
+        var con = eccent * sinphi;
+        var com = .5 * eccent;
+        con = Math.pow((1.0-con)/(1.0+con), com);
+        var ts = Math.tan(.5 * (Math.PI*0.5 - phi))/con;
+        var y = 0 - r_major * Math.log(ts);
+        return y;
+    }
+    function merc(x,y) {
+        return [merc_x(x),merc_y(y)];
+    }
     window.AnswerListMap = Backbone.View.extend({
         el: "#answerListMapContainer",
         
@@ -37,7 +65,14 @@ $(function () {
 	    for (i in entries) {
                 entry = entries[i];
                 ext = entry["bounds"];
-                bounds = OpenLayers.Bounds.fromArray(ext,true);
+                mercator = merc(ext[1], ext[0])
+                ext[0] = mercator[0]
+                ext[1] = mercator[1]
+                mercator = merc(ext[3], ext[2])
+                ext[2] = mercator[2]
+                ext[3] = mercator[3]
+                
+                bounds = OpenLayers.Bounds.fromArray(ext);
                 box = new OpenLayers.Marker.Box(bounds);
                 box.events.register("click", box, function (e) {
                     this.setBorder("yellow");
