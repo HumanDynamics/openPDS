15a30804329c4216d8de8b202d1ab96de2a4b4d3
brian717 <brian717@mit.edu>
2013-02-15 14:31:35 -0500

    Quick implementation of answer rating with test on social health radial page

---
+++ oms_pds/static/js/answerRating.js
@@ -0,0 +1,20 @@
+window.AnswerRatingTextView = Backbone.View.Extend({
+    tagName: "div", 
+    
+    initialize: function (options) {
+        _.bindAll(this, "render");
+        this.ratingListCollection = new AnswerListCollection({ key: options.key });
+        this.ratingListCollection.bind("reset", this.render);
+        this.ratingListCollection.fetch();
+    }, 
+    
+    render: function () {
+        var entriesModel = this.ratingListCollection.at(0);
+        var me = this;
+        $(this.el).append($("<input type='text' />").change(function () { 
+            entriesModel.get("value").push({time: (new Date).getTime() * 1000,  value: $(this).text() });
+            entriesModel.save();
+        }) );
+    }
+
+});
