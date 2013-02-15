3a47b70afa1c4103fbea66377c3216e3106ef1f5
brian717 <brian717@mit.edu>
2013-02-15 17:09:03 -0500

    Added first pass of star-rating control

--- oms_pds/static/js/answerRating.js
+++ oms_pds/static/js/answerRating.js
@@ -17,5 +17,35 @@ window.AnswerRatingTextView = Backbone.View.extend({
             entriesModel.save();
         }) );
     }
+});
 
+window.AnswerRatingStarView = AnswerRatingTextView.extend({
+   tagName: "div",
+   
+   render: function () {
+       var entriesModel = this.ratingListCollection.at(0);
+       entriesModel || (entriesModel = new AnswerListColleciton ({ key: this.options.key, value: [] }));
+       var me = this;
+       this.selectedRating = 0;
+       this.averageRating = Math.min(5, Math.max(0, entriesModel.value.reduce(function (sum, num) { return sum + num;}, 0) / entriesModel.value.length));
+       
+       for (i = 1; i <= this.averageRating; i++) {
+           this.appendStar(entriesModel, i, "/static/img/star_3.png");
+       }
+       
+       for (i = Math.min(5, Math.floor(this.averageRating + 1)); i <= 5; i++) {
+           this.appendStar(entriesModel, i, "/static/img/star_3.png");
+       }
+       
+   },
+   
+   appendStar: function (entriesModel, value, imgSrc) {
+       var me = this;
+       $(this.el).append($("<img src='"+imgSrc+"' />").click(function () { me.saveRating(entriesModel, value); }));
+   },
+   
+   saveRating: function (entriesModel, value) {
+       entriesModel.get("value").push({time: (new Date).getTime() / 1000, value: value });
+       entriesModel.save();
+   }
 });
