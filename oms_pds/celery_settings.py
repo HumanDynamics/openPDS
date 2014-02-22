7c4673bc263a59205976dc911f96e15a5a19dd92
SharonMP <brian717@mit.edu>
2014-02-21 19:20:42 -0500

    Committing social health celery settings for field trial server change

--- oms_pds/celery_settings.py
+++ oms_pds/celery_settings.py
@@ -1,23 +1,23 @@
 from celery.schedules import crontab
 
-CELERY_IMPORTS = ('oms_pds.tasks',"oms_pds.crowdsos_tasks")
-BROKER_URL = "mongodb://celery:celery@localhost:27017/crowdsos_celery_broker"
+CELERY_IMPORTS = ('oms_pds.tasks',"oms_pds.socialhealth_tasks")
+BROKER_URL = "mongodb://celery:celery@localhost:27017/lpd_celery_broker"
 CELERYBEAT_SCHEDULE = {
 #    "check-data-and-notify": {
 #        "task": "oms_pds.tasks.checkDataAndNotify", 
 #        "schedule": crontab(hour="*", minute="0")
 #    },
-    "ensure-incident-indexes": {
-        "task": "oms_pds.crowdsos_tasks.ensureIncidentIndexes",
-        "schedule": crontab(hour="*/2", minute="15")
-    },
-    "find-recent-incidents": {
-        "task": "oms_pds.crowdsos_tasks.findRecentIncidents",
-        "schedule": crontab(hour="*", minute="*")
-    },
+    "compute-social-health-scores": {
+        "task": "oms_pds.socialhealth_tasks.recentSocialHealthScores",
+        "schedule": crontab(hour="*", minute="*/30")
+     },
     "ensure-funf-indexes": {
         "task": "oms_pds.tasks.ensureFunfIndexes",
-        "schedule": crontab(hour="*/2", minute="45")
+        "schedule": crontab(hour="*/2", minute="15")
+    },
+    "find-recent-places": {
+        "task": "oms_pds.tasks.findRecentPlaces", 
+        "schedule": crontab(hour="*/2", minute="0")
     }
 }
 
