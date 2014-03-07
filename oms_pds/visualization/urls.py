c871e329c395143efe77af5d5254d6e97221e583
brian717 <brian717@media.mit.edu>
2014-03-07 16:08:17 -0500

    Added meetup_home to visualization urls - makes it work with MIT Mobile

--- oms_pds/visualization/urls.py
+++ oms_pds/visualization/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
-
+from oms_pds.meetup.views import meetup_home
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,5 +14,6 @@ urlpatterns = patterns('oms_pds.visualization.views',
     (r'^social', direct_to_template, { 'template' : 'visualization/social.html' }),
     (r'^focus', direct_to_template, { 'template' : 'visualization/focus.html' }),
     (r'^places', "places"),
+    (r'^meetup_home', meetup_home),
 #    (r"^places", direct_to_template, { "template" : "visualization/locationMap.html" })
 )
