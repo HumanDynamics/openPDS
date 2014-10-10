8ad38acd6a775386b080fecdf367be197487a8d9
Albert Carter <albert.r.carter@gmail.com>
2014-10-10 10:26:52 -0400

    fix settings.py and wsgi.py templates for relative directories

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 import sys
 import os
 
-
+print "\n"
+print "##########################################################################"
 print "NOTE: This must be run from the root directory of the openPDS project"
+print "and for the virtualenv to be located in the directory above it."
+print "ie: you should be in /pdsEnv/openPDS"
+print "if necessary, press ctrl+c to exit and then move to the correct directory."
+print "##########################################################################"
 virtualEnvPath = os.path.dirname(os.getcwd())
 virtualEnvPathInput = raw_input("Enter the path to the openPDS virtual environment (or nothing for default: %s): "%virtualEnvPath)
 virtualEnvPath = virtualEnvPathInput if virtualEnvPathInput is not None and len(virtualEnvPath) > 0 else virtualEnvPath
 
-registryServer = raw_input("Enter the Registry Server domain name (example: linkedpersonaldata.org): ")
+mitRegistryServer = "linkedpersonaldata.org"
+registryServerInput = raw_input("\nEnter the Registry Server domain name (or nothing for MIT default: %s): " %mitRegistryServer)
+registryServer = registryServerInput if registryServerInput is not None and len(registryServerInput) > 0 else mitRegistryServer
 
 # Create a new wsgi.py file from the template and fill in the virtual env path on it
 wsgiTemplateFile = open(os.getcwd() + "/openpds/wsgi.py.template", "r")
@@ -23,7 +30,7 @@ wsgiTemplateFile.close()
 
 backends = { "1": "openpds.backends.mongo", "2": "openpds.backends.sqlite", "3": "openpds.backends.postgres" }
 
-print "Which backend would you like to use for personal data storage?"
+print "\nWhich backend would you like to use for personal data storage?"
 print "1. MongoDB (openpds.backends.mongo)"
 print "2. SQLite (openpds.backends.sqlite)"
 print "3. Postsgres (openpds.backends.postgres)"
