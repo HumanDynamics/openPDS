141aa1803ab11aeb47143297641d076c2a84a241
chieni <mitchieni@gmail.com>
2014-10-28 13:29:39 -0400

    Reformatted json to reflect existing format

--- openpds/views.py
+++ openpds/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
+import os
 import json
 from django.template import RequestContext
 from django.shortcuts import render_to_response
@@ -8,6 +9,8 @@ import httplib
 from django import forms
 import json
 import pdb
+from django.templatetags.static import static
+
 
 def home(request):
     template = {}
@@ -21,7 +24,31 @@ def ping(request):
     return HttpResponse(json.dumps(response), mimetype="application/json")
 
 def lab_information(request):
-    labs = ["Social Health Tracker", "My Places", "Meetup", "MIT FIT"]
-    lab_info = {"Labs": labs, "Social Health Tracker": ["../static/img/star_1.png", "Keep track of your social activity throughout the day.", "Brian Sweatt"], "My Places": ["../static/img/star_1.png", "Keep track of your most visited places.", "Brian Sweatt"], "Meetup": ["../static/img/star_1.png", "Organize meetups with other people.", "Brian Sweatt"], "MIT FIT": ["../static/img/star_1.png", "MIT FIT is a fitness application.", "Sharon Paradesi"]}
-    json_labs = json.dumps(lab_info)
+    labs = [
+            {
+                'about': 'Social Health Tracker is an application to construct 3 social health metrics of an individual: activity, social, and focus.',
+                'credits': 'Brian Sweatt',
+                'name': 'Social Health Tracker',
+                'icon': '/static/img/star_1.png'
+            },
+            {   
+                'about': 'My Places is an application to mine frequently-visited locations and identify work and home locations.',
+                'credits': 'Brian Sweatt',
+                'name': 'My Places',
+                'icon': '/static/img/star_1.png'               
+            },
+            {   
+                'about': 'Meetup is an application to enable people schedule meetups without explicitly sharing their location or points of interest.',
+                'credits': 'Brian Sweatt, Sharon Paradesi, Ilaria Liccardi',
+                'name': 'Meetup',
+                'icon': '/static/img/star_1.png'             
+            },
+            {
+                'about': 'MIT-FIT enables users to track personal and aggregate high-activity regions and times, as well as view personalized fitness-related event recommendations.',
+                'credits': 'This lab was developed as part of the big data initiative at MIT led by Prof. Sam Madden and Elizabeth Bruce. The MIT-FIT lab was developed by Sharon Paradesi under the guidance of Dr. Lalana Kagal and with the help of Brian Sweatt. User experience and design suggestions were provided by Myra Hope Eskridge and Laura Watts.',               
+                'name': 'MIT-FIT',
+                'icon': '/static/img/star_1.png'
+            }    
+        ]
+    json_labs = json.dumps(labs)
     return HttpResponse(json_labs, mimetype="application/json")
